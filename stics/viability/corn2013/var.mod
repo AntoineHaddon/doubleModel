e9cb77d37a373cc20652e686690bacef9f34d5b7
Antoine Haddon <antoine.haddon@gmail.com>
2023-03-02 10:37:41 -0800

    gitignore

--- stics/viability/corn2013/var.mod
+++
@@ -1,8 +0,0 @@
-resmes
-AZnit(1)
-AZnit(2)
-AZnit(3)
-AZnit(4)
-AZnit(5)
-masec(n)
-codebbch_output
--- swan/bocophjb/maxBio_TotFerConstr_mlus/build/CMakeFiles/bocophjbcore.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/ahaddon/Programs/bocop/BocopHJB-1.1.0-Linux/core/lib/libbocophjbcore.a"
-)
